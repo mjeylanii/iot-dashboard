1b1bd0330941e84f6b58342d66cab6e287b1be3b
Mohamed Jeylani <95252871+mJeylanii@users.noreply.github.com>
2024-03-16 15:38:02 -0400

    Backup

---
+++ src/lib/components/settings/SelectField.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let label: string;
+	export let id: string;
+	export let value: string | number;
+	export let onChange: any;
+	export let options: any;
+</script>
+
+<div>
+	<label class="label" for={id}>
+		<span class="label-text">{label}</span>
+	</label>
+	<select class="select select-bordered w-full max-w-xs" {id} bind:value>
+		{#each options as option}
+			<option value={option.value}>{option.label}</option>
+		{/each}
+	</select>
+</div>
--- src/lib/components/settings/WebsocketSettings.svelte
+++ src/lib/components/settings/WebsocketSettings.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { getWebSocket } from '$lib/helpers/storageHelper';
+	import InputField from './InputField.svelte';
+	import SelectField from './SelectField.svelte';
+	import { setContext } from 'svelte';
+
 	let serverAddress = '';
 	let serverPort: number;
 	let serverUsername = '';
@@ -8,6 +12,8 @@
 	let serverProtocol = '';
 	let serverPath = '';
 	let topics: any;
+	let addNewDevice: boolean = false;
+
 	onMount(async () => {
 		let data = await getWebSocket();
 
@@ -21,81 +27,85 @@
 		topics = data.topics;
 	});
 
-	
+	const handleInputChange = (event: Event) => {
+		const { id, value } = event.target as HTMLInputElement;
+		switch (id) {
+			case 'server-address':
+				serverAddress = value;
+				break;
+			case 'server-port':
+				serverPort = parseInt(value, 10);
+				break;
+			case 'server-username':
+				serverUsername = value;
+				break;
+			case 'server-password':
+				serverPassword = value;
+				break;
+			case 'server-protocol':
+				serverProtocol = value;
+				break;
+			case 'server-path':
+				serverPath = value;
+				break;
+			default:
+				break;
+		}
+	};
 </script>
 
-<div class="form-control w-full max-w-md mx-auto tab-content items-center">
-	<label class="label" for="server-address">
-		<span class="label-text">Server address</span>
-	</label>
-	<input
-		type="text"
-		placeholder="127.0.0.1"
-		class="input input-bordered w-full max-w-xs"
+<div class="flex flex-col w-full tab-content items-center justify-center mx-auto px-16">
+	<InputField
+		label="Server address"
 		id="server-address"
 		value={serverAddress}
+		onChange={handleInputChange}
 	/>
-
-	<label class="label" for="server-port">
-		<span class="label-text">Server port</span>
-	</label>
-	<input
-		type="text"
-		placeholder="8080"
-		class="input input-bordered w-full max-w-xs"
+	<InputField
+		label="Server port"
 		id="server-port"
 		value={serverPort}
+		onChange={handleInputChange}
 	/>
-
-	<label class="label" for="server-username">
-		<span class="label-text">Server username</span>
-	</label>
-	<input
-		type="text"
-		placeholder="admin"
-		class="input input-bordered w-full max-w-xs"
+	<InputField
+		label="Server username"
 		id="server-username"
 		value={serverUsername}
+		onChange={handleInputChange}
 	/>
-
-	<label class="label" for="server-password">
-		<span class="label-text">Server password</span>
-	</label>
-	<input
-		type="password"
-		placeholder="password"
-		class="input input-bordered w-full max-w-xs"
+	<InputField
+		label="Server password"
 		id="server-password"
+		type="password"
 		value={serverPassword}
+		onChange={handleInputChange}
 	/>
-
-	<label class="label" for="server-protocol">
-		<span class="label-text">Server protocol</span>
-	</label>
-	<select class="select select-bordered w-full max-w-xs" id="server-protocol">
-		<option value="ws">ws</option>
-		<option value="wss">wss</option>
-	</select>
-
-	<label class="label" for="server-path">
-		<span class="label-text">Server path</span>
-	</label>
-	<input
-		type="text"
-		placeholder="/socket.io"
-		class="input input-bordered w-full max-w-xs"
+	<SelectField
+		label="Server protocol"
+		id="server-protocol"
+		value={serverProtocol}
+		onChange={handleInputChange}
+		options={[
+			{ label: 'ws', value: 'ws' },
+			{ label: 'wss', value: 'wss' }
+		]}
+	/>
+	<InputField
+		label="Server path"
 		id="server-path"
 		value={serverPath}
+		onChange={handleInputChange}
 	/>
+
 	<br />
-	<div class=" overflow-x-auto">
-		<table class="table w-full">
+	<div class=" overflow-x-auto max-w-md px-2">
+		<table id="websocket-devices-table" class="table w-full max-w-md">
 			<!-- head -->
 			<thead>
 				<tr>
 					<th />
-					<th>Name</th>
-					<th>Path</th>
+					<th class="w-full">Name</th>
+					<th class="w-full">Path</th>
 					<th />
 				</tr>
 			</thead>
@@ -111,10 +121,35 @@
 						</tr>
 					{/each}
 				{/if}
+				{#if addNewDevice}
+					<tr>
+						<td>
+							<input
+								type="text"
+								placeholder="Device"
+								class="input input-xs input-bordered mx-w-xs"
+								id="device-name"
+							/>
+						</td>
+						<td>
+							<input
+								type="text"
+								placeholder="/socket.io"
+								class="input input-xs input-bordered"
+								id="server-path"
+							/>
+						</td>
+						<td>
+							<button class="btn btn-xs btn-accent max-w-xs text-base-100">Add</button>
+						</td>
+					</tr>
+				{/if}
 			</tbody>
 			<tfoot>
-				<th colspan="4">
-					<button class="w-full btn btn-md">Add Device</button>
+				<th class="" colspan="4">
+					<button on:click={() => (addNewDevice = !addNewDevice)} class="w-full btn btn-md"
+						>Add Device</button
+					>
 				</th>
 			</tfoot>
 		</table>
