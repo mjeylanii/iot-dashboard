d27ec29629c7d702180176d7843a87daec0db236
Mohamed Jeylani <95252871+mjeylanii@users.noreply.github.com>
2024-06-24 09:29:03 -0400

    remove nmap

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,12 +1,5 @@
 use std::env;
 
 fn main() {
-    let project_dir = env::current_dir().unwrap();
-    let nmap_path = project_dir.join("nmap.exe");
-    println!(
-        "Copying nmap binary from {} to build directory",
-        nmap_path.display()
-    );
-    std::fs::copy(nmap_path, "build/nmap").unwrap();
     tauri_build::build();
 }
